5f5540ba0ca64800ff24927aa0ff5676eb213864
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-12-05 21:00:46 +0100

    starting to allocate the data to dataframe and export to excel

--- webscraper_imoveis.py
+++ webscraper_imoveis.py
@@ -2,32 +2,46 @@ import cloudscraper
 from bs4 import BeautifulSoup
 import pandas as pd
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 inicio_endereco_1 = 'https://www.idealista.com/alquiler-viviendas/barcelona-barcelona/'
 inicio_endereco_2 = 'https://www.idealista.com/alquiler-viviendas/madrid-madrid/'
 inicio_endereco_3 = 'https://www.idealista.com/alquiler-viviendas/cordoba-cordoba/'
 
 webpages_alquiler = [inicio_endereco_1, inicio_endereco_2, inicio_endereco_3]
-quant_paginas = 10
+quant_paginas = 20 #melhorar
 fim_endereco = '.htm'
 
-conjunto_anuncios_cidades = []
-for pagina_1 in webpages_alquiler:
-    variavel_cidade = pagina_1.split('/')[4]
+todos_anuncios = []
+anuncios_por_cidade = {}
+cidades = []
+
+
+# criando listas de paginas contendo os anuncios por cidade e unindo-as em uma coleção
+for pagina_principal in webpages_alquiler:
+    variavel_cidade = pagina_principal.split('/')[4]
     nome_cidade = variavel_cidade.split('-')[0]
-    todas_paginas_cidade = [pagina_1]
+    cidades.append(nome_cidade)
+    paginas_cidade = [pagina_principal]
 
     for i in range(2,quant_paginas):
-        endereco_completo = pagina_1 + f'pagina-{i}' + fim_endereco
-        todas_paginas_cidade.append(endereco_completo)
-    conjunto_anuncios_cidades.append(todas_paginas_cidade)
-print(conjunto_anuncios_cidades)
+        endereco_completo = pagina_principal + f'pagina-{i}' + fim_endereco
+        paginas_cidade.append(endereco_completo)
+    todos_anuncios.append(paginas_cidade)
+    # permitindo acessar, fora do loop, as listas de paginas por cada cidade em separado
+    anuncios_por_cidade.update({nome_cidade: paginas_cidade})
 
-# inicio =
+print(todos_anuncios)
+d = {'NOME': cidades}
+df = pd.DataFrame(d)
+print(df)
+
 
 # Turn all this below into a function so I can use it to any address (in idealista, at least)
 def coletar_dados_organizados(endereco):
     # endereco = input('Insira o endereço completo da página')
-
+    endereco = endereco
     scraper = cloudscraper.create_scraper()
     html = scraper.get(endereco).content
     soup1 = BeautifulSoup(html, 'html.parser')
@@ -48,12 +62,26 @@ def coletar_dados_organizados(endereco):
         # dict = {index: [titulo,preco_numerico,detalhes]}
         # print(dict)
     print(len(anuncios))
-# for p in webpages_alquiler:
-#     coletar_dados_organizados(p)
+
+
+# # coletando todas as páginas de todas as cidades.
+# for p in todos_anuncios:
+#     for item in p:
+#         coletar_dados_organizados(item)
+
+df.to_excel('test.xlsx', sheet_name=nomes_cidades[0])
+
 
 # Funçâo para coletar apenas textos dos objetos bs4 (não foi necessária a partir de quando se descobriu o item certo que
 # armazena cada anuncio. A ideia era coletar separadamente as listas de precos, titulos, area e depois referencia-las
 # de novo a seus respectivos anuncios a partir de seus indices em comum.
+
+
+
+
+
+
+
 
 def coletar_textos(soup_iteravel):
     lista_textos = []
